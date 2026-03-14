ac3af8a40cde2969052acab200d29f5085d79169
yoonchanyeong <dreampathway2004@gmail.com>
2026-03-15 04:12:45 +0900

    Create room.py

---
+++ python/app/schemas/room.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import List
+
+class CreateDMRoomRequest(BaseModel):
+    room_name: str
+    user1_uuid: str
+    user2_uuid: str
+
+class CreateTeamRoomRequest(BaseModel):
+    room_name: str
+    creator_uuid: str
+    members: List[str]
+
+class CreateTeamFromRoomRequest(BaseModel):
+    creator_uuid: str
+    room_name: str
+    new_members: List[str]
