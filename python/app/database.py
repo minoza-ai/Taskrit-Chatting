c17fd9db5340500d09cd2388dbc45b1affa588c7
yoonchanyeong <dreampathway2004@gmail.com>
2026-03-15 23:04:34 +0900

    Update database.py

--- python/app/database.py
+++ python/app/database.py
@@ -9,3 +9,12 @@ users_collection: Collection = db["users"]
 rooms_collection: Collection = db["rooms"]
 messages_collection: Collection = db["messages"]
 read_status_collection: Collection = db["read_status"]
+
+
+def create_indexes():
+    users_collection.create_index("user_uuid", unique=True)
+    rooms_collection.create_index("room_id", unique=True)
+    rooms_collection.create_index("dm_key")
+    messages_collection.create_index("message_id", unique=True)
+    messages_collection.create_index([("room_id", 1), ("seq", 1)])
+    read_status_collection.create_index([("room_id", 1), ("user_uuid", 1)], unique=True)
