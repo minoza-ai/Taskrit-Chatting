abf0650bfb09ca4a2362cedc9ab54e539d8a4278
yoonchanyeong <dreampathway2004@gmail.com>
2026-03-15 23:05:05 +0900

    Update users.py

--- python/app/routers/users.py
+++ python/app/routers/users.py
@@ -4,9 +4,11 @@ from app.services.room_service import list_user_rooms_service
 
 router = APIRouter(tags=["users"])
 
+
 @router.get("/users")
 def get_users():
     return get_all_users()
+
 
 @router.get("/users/{user_uuid}/rooms")
 def list_user_rooms(user_uuid: str):
