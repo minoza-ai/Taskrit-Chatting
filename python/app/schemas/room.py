72248b7bef768aa851d7b9f720c023f7f0ba0bf5
yoonchanyeong <dreampathway2004@gmail.com>
2026-03-16 23:04:41 +0900

    Update room.py

--- python/app/schemas/room.py
+++ python/app/schemas/room.py
@@ -4,17 +4,14 @@ from typing import List
 
 class CreateDMRoomRequest(BaseModel):
     room_name: str
-    user1_uuid: str
-    user2_uuid: str
+    target_user_uuid: str
 
 
 class CreateTeamRoomRequest(BaseModel):
     room_name: str
-    creator_uuid: str
     members: List[str]
 
 
 class CreateTeamFromRoomRequest(BaseModel):
-    creator_uuid: str
     room_name: str
     new_members: List[str]
