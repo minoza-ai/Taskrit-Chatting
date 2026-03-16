330b1d0e372f546aad02a2a9c41788cc29e49ed1
yoonchanyeong <dreampathway2004@gmail.com>
2026-03-16 19:46:53 +0900

    Update main.py

--- python/app/main.py
+++ python/app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import APP_NAME
 from app.database import create_indexes
 from app.services.user_service import seed_users
-from app.routers import users, rooms, messages, files, reads
+from app.routers import room, messages, files, read
+from app.websocket import chat_ws
 
 app = FastAPI(title=APP_NAME)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def startup_event():
@@ -13,8 +22,8 @@ def startup_event():
     seed_users()
 
 
-app.include_router(users.router)
-app.include_router(rooms.router)
+app.include_router(room.router)
 app.include_router(messages.router)
 app.include_router(files.router)
-app.include_router(reads.router)
+app.include_router(read.router)
+app.include_router(chat_ws.router)
